a45612e7002bc3f0efa80d8f90809f2a4f6916a4
seyeong <seyoung7555@naver.com>
2022-09-13 18:21:25 +0900

    feat: DB 연동해서 Hello World 잡 실행

--- src/main/java/io/spring/batch/BatchApplication.java
+++ src/main/java/io/spring/batch/BatchApplication.java
@@ -1,10 +1,39 @@
 package io.spring.batch;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Hello, world!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
