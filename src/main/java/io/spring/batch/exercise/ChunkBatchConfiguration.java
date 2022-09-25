f5137a101a87394e6ae620029b1b280c93749c1e
Seyeong kim <seyoung7555@naver.com>
2022-09-26 00:26:03 +0900

    feat: ChunkBatchConfiguration 구현

--- src/main/java/io/spring/batch/exercise/ChunkBatchConfiguration.java
+++ src/main/java/io/spring/batch/exercise/ChunkBatchConfiguration.java
@@ -6,8 +6,17 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.PassThroughLineMapper;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @RequiredArgsConstructor
 @EnableBatchProcessing
@@ -21,16 +30,38 @@ public class ChunkBatchConfiguration {
     public Job chunkBatch() {
         return this.jobBuilderFactory
                 .get("chunkBatch")
-                .start(step1())
+                .start(chunkStep())
                 .build();
     }
 
     @Bean
-    public Step step1() {
-        return this.stepBuilderFactory.get("step1")
-                .chunk(10)
+    public Step chunkStep() {
+        return this.stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(10)
                 .reader(itemReader(null))
                 .writer(itemWriter(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<String> itemReader(
+            @Value("#{jobParameters['inputFile']}") Resource inputFile) {
+        return new FlatFileItemReaderBuilder<String>()
+                .name("itemReader")
+                .resource(inputFile)
+                .lineMapper(new PassThroughLineMapper())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<String> itemWriter(
+            @Value("#{jobParameters['outputFile']}") Resource outputFile) {
+        return new FlatFileItemWriterBuilder<String>()
+                .name("itemWriter")
+                .resource(outputFile)
+                .lineAggregator(new PassThroughLineAggregator<>())
                 .build();
     }
 }
